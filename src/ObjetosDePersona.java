c1a051d9d444920dd99fd9c10d3248dd379ec61f
AmazingJuan <157389739+AmazingJuan@users.noreply.github.com>
2024-02-26 11:03:53 -0500

    Update ObjetosDePersona.java

--- src/ObjetosDePersona.java
+++ src/ObjetosDePersona.java
@@ -17,7 +17,7 @@ public class ObjetosDePersona {
         //SE HACE USO DE LA HERENCIA, LOS DATOS CREADOS ADOPTAN LAS ACCIONES Y CARACTERISTICAS DE LA CLASE 'Persona'.
         Persona persona1 = new Persona("Mike", (short) 16, "Estudiante", 1.75f, 70f); //Se crea una instancia de la clase Persona
         Persona persona2 = new Persona("Ernesto", (short) 34, "Docente", 1.70f, 80f); // Se crea una instancia de la clase Persona
-        Persona persona3 = new Persona("Carlos", (short) 60, "Administrativo", 1.60f, 75f); //Se crea una instancia de la clase Persona (por el segundo constructor)
+        Persona persona3 = new Persona("Carlos", (short) 60, "Administrativo", 1.60f, 100f); //Se crea una instancia de la clase Persona (por el segundo constructor)
         Persona persona4 = new Persona("Maria");
         Utilidades.limpiarPantalla(); //Limpia la pantalla;
         System.out.println("========================= BIENVENIDA =========================\n\nBienvenido al programa, usted verá a distintas personas hacer acciones a continuación. ");
